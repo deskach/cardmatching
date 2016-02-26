35fe7b042695d9503349119ecd5543cf8278140b
Dzianis Kachanau <dzianis.kachanau@autogeneral.com.au>
2016-02-26 11:43:44 +1000

    Introduced card.component.css

--- app/card/card.component.ts
+++ app/card/card.component.ts
@@ -3,6 +3,7 @@ import {Card} from "./card";
 
 @Component({
     selector: "cm-card",
+    styleUrls: ['app/card/card.component.css'],
     inputs: ['card'],
     template: `<div class="cm-card">{{card.isFolded ? '#' : card.id}}</div>`
 })
