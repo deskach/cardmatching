d34f3522c8b85707741a4bc0fdaeaaaac00f7401
Dzianis Kachanau <dzianis.kachanau@autogeneral.com.au>
2016-02-29 11:55:07 +1000

    Introduced a 1sec delay before the card is folded

--- app/card/card.ts
+++ app/card/card.ts
@@ -1,10 +1,24 @@
 export class Card {
-    public isFolded: boolean = true;
+    private _isFolded:boolean = true;
+
+    get isFolded():boolean {
+        return this._isFolded;
+    }
+
+    set isFolded(aIsFolded:boolean) {
+        if(aIsFolded) { // delay card folding, to give some time to a user to see its face
+            setTimeout(() => {
+                this._isFolded = true;
+            }, 1000);
+        } else {
+            this._isFolded = false;
+        }
+    }
 
     constructor(public id:number) {
     }
 
-    clone() {
+    clone():Card {
         return new Card(this.id);
     }
 }
