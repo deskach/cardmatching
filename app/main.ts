820bf5743a4fe5c9a52ae24b09826c8838466e20
Dzianis Kachanau <dzianis.kachanau@autogeneral.com.au>
2016-03-04 16:14:33 +1000

    Included typings information

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,5 @@
+/// <reference path="../node_modules/angular2/typings/browser.d.ts" />
+
 import {AppComponent}     from './app.component';
 import {bootstrap}        from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
