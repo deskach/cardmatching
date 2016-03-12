07b639c4cf47857d21906e8beb46a251d5deca42
deskach <dzianis.kachanau@gmail.com>
2016-03-12 13:02:44 +1000

    Added Apply and Cancel buttons into the Settings

--- app/settings/settings.component.ts
+++ app/settings/settings.component.ts
@@ -2,6 +2,7 @@ import {Component}   from 'angular2/core';
 import {GameSettings} from "./settings";
 
 @Component({
+    styleUrls: ['app/settings/settings.css'],
     templateUrl: 'app/settings/settings.component.html'
 })
 export class SettingsComponent {
--- app/settings/settings.ts
+++ app/settings/settings.ts
@@ -1,5 +1,5 @@
 export class GameSettings {
-    static gameTypes = ['Match pictures', 'Match a letter and a picture', 'Match letters'];
+    static gameTypes = ['Pictures', 'Letters'];
 
-    gameType = GameSettings.gameTypes[0];
+    cardTypes = [GameSettings.gameTypes[0], GameSettings.gameTypes[0]];
 }
