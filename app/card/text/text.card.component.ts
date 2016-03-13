93c17eab8a11c946234ac617b068e09ee4d64e39
deskach <dzianis.kachanau@gmail.com>
2016-03-13 12:56:05 +1000

    Restiled text card

--- app/card/text/text.card.component.ts
+++ app/card/text/text.card.component.ts
@@ -3,9 +3,17 @@ import {TextCard} from "./text.card";
 
 @Component({
     selector: "crm-text-card",
-    styleUrls: ['app/card/card.css'],
+    styleUrls: ['app/card/card.css', 'app/card/text/text.card.css'],
     inputs: ['card'],
-    template: `<div class="crm-card thumbnail">{{card.isFolded ? '' : card.text}}</div>`
+    template: `
+    <div class="crm-card thumbnail">
+        <div *ngIf="!card.isFolded" >
+            <div class="crm-text-card">
+                {{card.text}}
+            </div>
+        </div>
+    </div>
+    `
 })
 export class TextCardComponent {
     public card: TextCard;
