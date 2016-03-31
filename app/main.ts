2ab9438f673ddee3ad452949d4815b899467a97d
Dzianis Kachanau <dzianis.kachanau@autogeneral.com.au>
2016-03-31 16:27:48 +1000

    Enabled prod mode

--- app/main.ts
+++ app/main.ts
@@ -3,6 +3,9 @@
 import {AppComponent}     from './app.component';
 import {bootstrap}        from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
+import {enableProdMode} from "angular2/core";
+
+enableProdMode();
 
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS
